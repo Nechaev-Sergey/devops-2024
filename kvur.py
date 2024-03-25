32afcf3db5d08c89bc43634146ac035f5ecd1213
SNechaev <snechaev@lanit.ru>
2024-03-25 11:03:31 +0300

    заставил пользоателя вводить корректные данные, обработал ошибки

--- kvur.py
+++ kvur.py
@@ -1,1 +1,16 @@
 print ("Решаем квадратное уравнение")
+print("Для решения уравнения введите следующие значения как число")
+bad_data = True
+while bad_data == True:
+    try:
+        a = int(input("Введите a:"))
+        b = int(input("Введите b:"))
+        c = int(input("Введите c:"))
+        bad_data = False
+
+    except ValueError:
+        print("Ошибка данных")
+    except ZeroDivisionError:
+        print("На ноль делить нельзя!")
+    except:
+        print("какая-то ошибка")
